ce0b36dc1776457bf8a58b4d1cb119387b645bd9
sohail.khan1 <sohail.khan1@cars24.com>
2023-12-07 03:42:10 +0530

    cors config added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 const express = require('express');
+const cors = require('cors');
 const checkIfCreditCardIsValid = require('./credit-card-utils');
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/public/health", (req,res) => {
     res.status(200).send({
